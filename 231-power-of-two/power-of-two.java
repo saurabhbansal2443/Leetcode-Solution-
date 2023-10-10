6f9627f0e422826eebaa1089fa11c4308b4cc4a9
SAURABH BANSAL <saurabhbansal2443@gmail.com>
2023-10-10 11:08:39 +0530

    Time: 1 ms (84.77%) | Memory: 39.5 MB (62.39%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int x) {
+       if(x==1 || x==2){
+           return true ;
+       }else if ( x%2==1 || x<1){
+           return false;
+       }
+
+       return isPowerOfTwo(x/2);
+    }
+}
